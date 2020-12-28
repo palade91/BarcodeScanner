1b4a6cfd557be55fa5d4457d5189af2d9ee89b90
Catalin Palade <catalin@recruitd.com>
2020-12-28 10:08:27 +0000

    refactoring name, added UI, added Camera Usage Description in Info.plist

---
+++ BarcodeScanner-SwiftUI/BarcodeScannerView.swift
@@ -0,0 +1,37 @@
+//
+//  BarcodeScannerView.swift
+//  BarcodeScanner-SwiftUI
+//
+//  Created by Catalin Palade on 28/12/2020.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer().frame(height: 60)
+                
+                Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text("Not Yet Scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+    }
+}
