2e7aaeb4e82182a2514db5d4b53fb04c4d5ac5ea
Catalin Palade <catalin@recruitd.com>
2020-12-28 09:43:28 +0000

    Initial Commit

---
+++ BarcodeScanner-SwiftUI/BarcodeScanner_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScanner_SwiftUIApp.swift
+//  BarcodeScanner-SwiftUI
+//
+//  Created by Catalin Palade on 28/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScanner_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarcodeScanner-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarcodeScanner-SwiftUI
+//
+//  Created by Catalin Palade on 28/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
